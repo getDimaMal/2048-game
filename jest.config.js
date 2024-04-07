81f6a5c46d57dc5c690f21d28f0a5ce1b3f7c409
Malyshev <getDimaMal@gmail.com>
2024-04-07 09:50:41 +0300

    Add Jest test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageReporters: ['clover'],
+    collectCoverageFrom: [
+        'src/**/*.{js,jsx,ts,tsx}',
+        '!src/**/index.{js,ts}',
+    ],
 };
